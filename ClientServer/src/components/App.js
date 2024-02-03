8c04bf1b3924e1cf8446df3e4a997dbb817e04a7
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-02 22:43:29 -0500

    webpack structure

--- ClientServer/src/components/App.js
+++ ClientServer/src/components/App.js
@@ -1,11 +1,7 @@
 
 
 function App() {
-    return (
-      <div>
-        <h1>React App</h1>
-      </div>
-    );
+    
 }
 
 module.exports = App;
--- ClientServer/src/index.js
+++ ClientServer/src/index.js
@@ -1,8 +1,8 @@
 const ReactDOM = require("react-dom/client");
-const App = require('./components/App.js');
+const App = require('./components/App');
 
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+//const root = ReactDOM.createRoot(document.getElementById("app"));
 
 
-root.render(<App />);
+//root.render(<App />);
