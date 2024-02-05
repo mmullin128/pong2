873d7faad4f7bc690a0f07ae5d78ff9803c391e5
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-05 17:55:37 -0500

    react main page

--- ClientServer/src/components/App.js
+++ ClientServer/src/components/App.js
@@ -30,7 +30,7 @@ export default function App() {
     console.log(currentPage);
     return (
         <div id="app">
-            {pages[currentPage]}
+            {pages[pageTree.length-1]}
         </div>
     );
 }
