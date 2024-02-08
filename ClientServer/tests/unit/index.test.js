842b0b341119ec01445ed974e1554d45120ba922
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-07 23:02:17 -0500

    tests

--- ClientServer/tests/unit/index.test.js
+++
@@ -1,6 +0,0 @@
-const { init, close } = require('../../dist/server/Server.js');
-
-test("server start and close", () => {
-    const PORT = 3000;
-    const server = init(PORT, { close: true });
-});
