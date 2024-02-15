9021099afdec6793feb91340bb38861169dfdeba
mattmulli <matthewunderscoremullin@gmail.com>
2024-02-15 03:51:13 -0500

    entrypoint

--- ClientServer/api/entrypoint.sh
+++ ClientServer/api/entrypoint.sh
@@ -2,9 +2,13 @@
 NODE_ENV=$(< $NODE_ENV_FILE)
 DB_URI=$(< $DB_URI_FILE)
 export DB_URI=$DB_URI
+
+printenv
+
 if [ "$NODE_ENV" == "TEST" ]
 then
     echo "Running Tests"
+    echo $ENV
     npm install --save-dev
     npm run test
 elif [ "$NODE_ENV" == "PROD" ]
